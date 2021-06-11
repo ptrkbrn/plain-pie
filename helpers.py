c9313a66f23ae09f56b87d59f3b771402fcd0460
Patrick Breen <pressure78@gmail.com>
2021-06-11 11:56:14 -0400

    finish basic functionality

---
+++ helpers.py
@@ -0,0 +1,3 @@
+def to_msec(timestamp):
+    return ((timestamp.hour * 60 + timestamp.minute) * 60 + timestamp.second) * 1000
+
--- parser.py
+++ parser.py
@@ -1,68 +1,130 @@
 import sys
 import os
 import cv2
+import base64
+from datetime import datetime
 from pysubparser import parser
 from pysubparser.cleaners import brackets
+from helpers import to_msec
 
-try:
-    path = sys.argv[1]
-    # parse through each file in specfied directory
-    for file in os.listdir(path):
-        ext = os.path.splitext(file)[1]
-        filepath = os.path.join(path, file)
-        # if subtitle file
-        if ext == '.srt':
-            subtitles = brackets.clean(
-                parser.parse(filepath)
-            )
-            for subtitle in subtitles:
-                print(subtitle.start)
-                print(subtitle.end)
-                print(subtitle.text)
-        # if video file
-        elif ext == '.mp4':
-            # reads video file
-            cam = cv2.VideoCapture(filepath)
-            # creates array of timestamps
-            timestamps = [cam.get(cv2.CAP_PROP_POS_MSEC)]
-            try:
-                # creates directory for video frames
-                if not os.path.exists(os.path.join(path, 'data')):
-                    os.makedirs('data')
-            # if directory is not created, throw error
-            except OSError:
-                print("Error: Could not create directory")
+import psycopg2
+from config import config
 
-            # frame
-            currentframe = 0
+def connect():
+    # Connect to db server
+    conn = None
+    try:
+        # read connection params
+        params = config()
 
-            while(True):
-                # read data from frame
-                ret, frame = cam.read()
+        # connect to Postgres server
+        print('Connecting to PostgreSQL database...')
+        conn = psycopg2.connect(**params)
 
-                if ret:
-                    # if video runtime still left, continue creating frames
-                    name = './data/frame' + str(currentframe) + '.jpg'
-                    print('Creating... ' + name)
-                    # adds current timestamp to array
-                    timestamps.append(cam.get(cv2.CAP_PROP_POS_MSEC))
-                    print(timestamps[currentframe] / 1000)
-                    #write extracted frames
-                    cv2.imwrite(name, frame)
+        # create cursor
+        cur = conn.cursor()
 
-                    # increment counter to update frame count
-                    currentframe += 1
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL db server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        try:
+            path = sys.argv[1]
+            # parse through each file in specfied directory
+            for file in os.listdir(path):
+                ext = os.path.splitext(file)[1]
+                filepath = os.path.join(path, file)
+                basename = os.path.basename(filepath)
+                filename = os.path.splitext(basename)[0]
+                # if subtitle file
+                if ext == '.srt':
+                    subtitles = brackets.clean(
+                        parser.parse(filepath)
+                    )
+                    for subtitle in subtitles:
+                        sql = """INSERT INTO subtitles(text, episode, time_start, time_end)
+                    VALUES(%s, %s, %s, %s);"""
+                        print(to_msec(subtitle.start))
+                        print(to_msec(subtitle.end))
+                        print(subtitle.text)
+                        print(sql, (subtitle.text, to_msec(subtitle.start), to_msec(subtitle.end),))
+                        print(cur.execute(sql, (subtitle.text, filename, to_msec(subtitle.start), to_msec(subtitle.end),)))
+                        conn.commit()
+                # if video file
+                elif ext == '.mkv':
+                    # reads video file
+                    cam = cv2.VideoCapture(filepath)
+                    # creates array of timestamps
+                    timestamps = [cam.get(cv2.CAP_PROP_POS_MSEC)]
+                    # frame
+                    currentframe = 0
+                    lastgreymean = 0
+                    # try:
+                    #     # creates directory for video frames
+                    #     if not os.path.exists(os.path.join(path, 'data')):
+                    #         os.makedirs('data')
+                    # # if directory is not created, throw error
+                    # except OSError:
+                    #     print("Error: Could not create directory")
+
+                    while(True):
+                        # read data from frame
+                        ret, frame = cam.read()
+                        if ret:
+                            # if video runtime still left, continue creating frames
+                            sql = """INSERT INTO screenshots(image_data, timestamp, episode, filename)
+                            VALUES(%s, %s, %s, %s);"""
+                            name = filename + '-' + str(currentframe) + '.jpg'
+                            # adds current timestamp to array
+                            timestamps.append(cam.get(cv2.CAP_PROP_POS_MSEC))
+                            # print(timestamps[currentframe] / 1000)
+                            #write extracted frames
+                            # print(cv2.subtract(lastframe, frame))
+                            # converts frame to greyscale
+                            grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                            # reads mean value of greyscale image
+                            grey_mean = cv2.mean(grey_frame)
+                            # evaluates motion blur in each frame
+                            if cv2.Laplacian(frame, cv2.CV_64F).var() > 60:
+                                # if mean different enough, i.e. frame is unique enough, continue
+                                difference = cv2.subtract(grey_mean, lastgreymean)
+                                if difference[0][0] > .8 or difference[0][0] < -.8:
+                                    print('Creating... ' + name)
+                                    # increment last meme with current frame
+                                    lastgreymean = cv2.mean(grey_frame)
+                                    # encode image as string and add to db
+                                    img_str = cv2.imencode('.jpg', frame)[1].tostring()
+                                    cur.execute(sql, (img_str, timestamps[currentframe], filename, name))
+                                    conn.commit()
+
+                            # increment counter to update frame count
+                            currentframe += 1
+                        else:
+                            break
+                    
+                    # Release space and windows once done
+                    cam.release()
+                    cv2.destroyAllWindows()
+
+                # if not supported file type
                 else:
-                    break
-            
-            # Release space and windows once done
-            cam.release()
-            cv2.destroyAllWindows()
+                    print('Not a valid file type')
+        except:
+            print('Not a valid directory')
 
-        # if not supported file type
-        else:
-            print('Not a valid file type')
-except:
-    print('Not a valid directory')
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 
 
+if __name__ == '__main__':
+    connect()
